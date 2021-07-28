23ee4e55b8dfb7f72872e64c1d4a0e28f62a0491
Velkur Bharath <velkurbharath1@gmail.com>
2021-07-28 14:33:46 +0530

    Update checkmypass.py

--- PasswordCheckPy/checkmypass.py
+++ PasswordCheckPy/checkmypass.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 import hashlib
 
@@ -10,10 +11,10 @@ def request_api_data(query_char):
     return res
 
 
-def get_password_leaks_count(hashes,hash_to_check):
-    hashes=(line.split(':')for line in hashes.text.splitlines())
-    for h,count in hashes:
-        if h==hash_to_check:
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
             return count
     return 0
 
@@ -23,17 +24,19 @@ def pwned_api_check(password):
     first_5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first_5_char)
     print(response)
-    return get_password_leaks_count(response,tail)
+    return get_password_leaks_count(response, tail)
+
 
 def main(args):
     for password in args:
-        count=pwned_api_check(password)
+        count = pwned_api_check(password)
         if count:
             print(f'{password} was found {count}times...you should change your password')
         else:
             print(f'{password} was not found.carry on!')
-    return '_done!_'    
+    return '_done!_'
 
 
-if__name__=='__main__':
-   sys.exit(main(sys.argv[1:]))
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
+
